78c1b05c93a1a522c182918e909c0f60b369d2f5
Joey Rosztoczy <joeyr@stotzeq.com>
2016-10-18 11:52:03 -0700

    sass and loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,42 @@
+var CopyWebpackPlugin = require("copy-webpack-plugin");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-  entry: ["./web/static/css/app.css", "./web/static/js/app.js"],
+  entry: ["./web/static/css/app.scss", "./web/static/js/app.js"],
   output: {
-    path: "./priv/static/js",
+    path: "./priv/static",
     filename: "app.js"
+  },
+  resolve: {
+    modulesDirectories: [ "node_modules", __dirname + "/web/static/js" ]
   },
   module: {
     loaders: [{
         test: /\.js$/,
         exclude: /node_modules/,
-        loaders: ["babel"],
-        include: __dirname,
+        loader: "babel",
         query: {
           presets: ["es2015"]
         }
     }, {
+      test:    /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+      loader: 'url?limit=1000',
+    }, {
+      test:    /\.(png|gif|jpg)(\?[a-z0-9]+)?$/,
+      loader: 'url-loader?limit=100000',
+    }, {
       test: /\.css$/,
       loader: ExtractTextPlugin.extract("style", "css")
-    }]
+    }, {
+      test: /\.scss$/,
+      loader: ExtractTextPlugin.extract(
+        'style',
+        'css!sass?includePaths[]=' + __dirname + '/node_modules'
+      )
+    }],
   },
   plugins: [
-    new ExtractTextPlugin("css/app.css")
-  ],
-  resolve: {
-    modulesDirectories: [ "node_modules", __dirname + "/web/static/js" ]
-  }
+    new ExtractTextPlugin("css/app.css"),
+    new CopyWebpackPlugin([{ from: "./web/static/assets" }])
+  ]
 };
