65ef443323814dd02f7896a1e666e6cd25ea6d34
Joey Rosztoczy <joeyr@stotzeq.com>
2016-10-18 13:33:00 -0700

    application process

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,6 +1,10 @@
 defmodule FoundationX.PageController do
   use FoundationX.Web, :controller
 
+  def application(conn, _params) do
+    render conn, "application.html"
+  end
+
   def index(conn, _params) do
     render conn, "index.html"
   end
