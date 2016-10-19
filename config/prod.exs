e6be737b8f530690a8f6e24fac3b7146888e50ff
Joey Rosztoczy <joeyr@stotzeq.com>
2016-10-19 11:35:45 -0700

    remove https until ssl

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :foundation_x, FoundationX.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "www.rosztoczyfoundation.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [scheme: "http", host: "www.rosztoczyfoundation.com", port: 443],
+  # force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
